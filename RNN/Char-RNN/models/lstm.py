ac51193c97158601d53b27851d0177d1a2113a07
Phan Nhat Hoang <johnphan1608@gmail.com>
2023-02-03 15:19:36 +0800

    Added eval loop to RNN/Char-RNN/train.py and RNN/Char-RNN/models/lstm.py

---
+++ RNN/Char-RNN/models/lstm.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LSTM(nn.Moudle):
+  def __init__(self, dim_input, dim_hidden, dim_output):
+    super().__init__()
+    self.dim_input = dim_input
+    self.dim_hidden = dim_hidden
+    self.dim_output = dim_output
+
+    self.ih2F = nn.Linear(dim_input+dim_hidden, dim_hidden)
+    self.ih2I = nn.Linear(dim_input+dim_hidden, dim_hidden)
+    self.ih2C = nn.Linear(dim_input+dim_hidden, dim_hidden)
+    self.ih2O = nn.Linear(dim_input+dim_hidden, dim_hidden)
+    self.sigmoid = nn.Sigmoid()
+    self.tanh = nn.Tanh()
+    self.ic2o = nn.Linear(dim_input+dim_hidden, dim_output)
+    self.softmax = nn.LogSoftmax(dim=1)
+  
+  def forward(self, input, H_C):
+    if H_C is None:
+      H = torch.zeros((input.shape[0], dim_hidden)),
+      C = torch.zeros((input.shape[0], dim_hidden)) 
+    else:
+      H, C = H_C
+    # input is in the size of (batch_size, dim_input)
+    # dim_hidden is in the size of (batch_size, dim_hidden)
+    # memory_cell is in the size of (batch_size, dim_hidden)
+    combined = torch.concat((input, hidden), dim=1)
+    F_gate = self.sigmoid(self.ih2F(combined))
+    I_gate = self.sigmoid(self.ih2I(combined))
+    C_gate = self.tanh(self.ih2C(combined))
+    O_gate = self.sigmoid(self.ih2O(combined))
+
+    C_new = memory_cell * F_gate + I_gate * C_gate
+    H = self.tanh(C_new) * O_gate
+    output = self.ic2o(torch.concat((input, H), dim=1))
+    output = self.softmax(output)
+    
+    return output, (H, C_new)
+
+  def init_hidden(self):
+    return torch.zeros((1, self.dim_hidden))
--- RNN/Char-RNN/train.py
+++ RNN/Char-RNN/train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from model import RNN
+from models import RNN, LSTM
 from data import read_data, split_data, Dataloader
 
 class TrainingArgs:
@@ -9,10 +9,11 @@ class TrainingArgs:
     self.num_epochs = num_epochs
 
 class Trainer:
-  def __init__(self, model=None, training_args=None, data_loader=None):
+  def __init__(self, model=None, training_args=None, data_loader_train=None, data_loader_val=None):
     self.args = training_args
     self.model = model
-    self.data_loader = data_loader
+    self.data_loader_train = data_loader_train
+    self.data_loader_val = data_loader_val
     self.loss = nn.NLLLoss()
   
   def train_step(self, input, label):
@@ -30,13 +31,33 @@ class Trainer:
     
     return output, loss.item()
 
+  def eval_step(self, input, label):
+    hidden = self.model.init_hidden()
+    for i in range(input.size())[0]:
+      output, hidden = self.model(input[i], hidden)
+    
+    loss = self.loss(output[0], label[0][0])
+    return output, loss
+
+  def eval(self, current_epoch):
+    total_loss = 0
+    correct_ans = 0
+    for input, label in self.data_loader_val:
+      output, loss = self.eval_step(input, label)
+      total_loss += loss
+      pred = F.one_hot([self.all_categories.index(torch.argmax(output, dim=1))], len(self.all_categories))
+      if label == pred:
+        correct_ans += 1
+    print("Current epoch has loss: ", total_loss)
+    print("Curren acc on val is: ", correct_ans/len(self.data_loader_val))
+  
   def train(self):
-    for batch in range(self.args.num_epochs):
+    for epoch in range(self.args.num_epochs):
       current_loss = 0
-      for input, label in self.data_loader:
+      for input, label in self.data_loader_train:
         output, loss = self.train_step(input, label)
         current_loss += loss
-      print(current_loss)
+      self.eval(epoch)
 
 def main():
   data_pth = "./data/names/*.txt"
@@ -46,9 +67,17 @@ def main():
     learning_rate=0.005,
     num_epochs=300
   )
-  data_loader = Dataloader(names=data['train']['names'], tags=data['train']['tags'], all_letters=all_letters, all_categories=all_categories)
+  data_loader_train = Dataloader(names=data['train']['names'], tags=data['train']['tags'], all_letters=all_letters, all_categories=all_categories)
+  data_loader_val = Dataloader(names=data['val']['names'], tags=data['val']['tags'], all_letters=all_letters, all_categories=all_categories)
   model = RNN(len(all_letters), 128, len(all_categories))
-  trainer = Trainer(model=model, training_args=training_args, data_loader=data_loader) 
+  
+  trainer = Trainer(
+    model=model, 
+    training_args=training_args, 
+    data_loader_train=data_loader_train, 
+    data_loader_val=data_loader_val
+  )
+
   trainer.train()
 
 if __name__ == "__main__":
